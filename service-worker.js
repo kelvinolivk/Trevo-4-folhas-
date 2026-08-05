eae307e8f3c4248e6108eed1a133a5e86d52efac
kelvinolivk <oliveirakelvin507@gmail.com>
2026-08-04 23:20:36 -0300

    Add service worker for file caching
    Implement service worker for caching files.

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,23 @@
+const CACHE_NAME = "trevo4folhas-v1";
 
+const arquivos = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(arquivos);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
