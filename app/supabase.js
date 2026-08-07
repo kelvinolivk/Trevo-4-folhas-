d7c1e328b6a8af122402c282fc08c441c5d5b7aa
kelvinolivk <oliveirakelvin507@gmail.com>
2026-08-07 19:06:08 -0300

    Initialize Supabase client with URL and key

--- app/supabase.js
+++ app/supabase.js
@@ -1,1 +1,8 @@
+const SUPABASE_URL = "https://qrvkpojkgtflilehkimh.supabase.co";
 
+const SUPABASE_KEY = "sb_publishable_JMQspHVULcC4hiJlyqPi4A_SAhd7Na0";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
